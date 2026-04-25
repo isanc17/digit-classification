16425d52d68760c7ce3a4f05c99df837a6c6b8b6
isanc17 <isanc17@bancodebogota.com.co>
2026-04-25 13:05:29 +0000

    feat: variables de entorno para descarga de modelo mlflow

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -4,8 +4,12 @@ class Settings(BaseSettings):
     APP_NAME: str = "MNIST Inferencia API"
     MODEL_PATH: str = "models/modelo_mnist.keras"
     ALLOWED_ORIGINS: list = ["http://localhost:3000", "http://localhost:8080"]
+    USE_MLFLOW_REGISTRY: bool = True
+    MODEL_ALIAS: str = "champion"
+    MLFLOW_TRACKING_URI: str = "http://localhost:8080"
     
     class Config:
         env_file = ".env"
+        env_file_encoding = 'utf-8'
 
 settings = Settings()
